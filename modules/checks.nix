3956eb1d8ed12c1848c7254cdaa6816ffc013762
ash-r640-0 <phsilbert@gmail.com>
2026-09-22 18:13:09 +0000

    feat(eVolver): add Codex developer tooling

--- modules/checks.nix
+++ modules/checks.nix
@@ -1,4 +1,26 @@
-_: {
+{
+  config,
+  lib,
+  ...
+}:
+let
+  eVolver = config.flake.nixosConfigurations.eVolver.config;
+  packageNames = map (
+    package: package.pname or package.name or ""
+  ) eVolver.environment.systemPackages;
+  requiredPackageNames = [
+    "codex"
+    "codex-switch"
+    "docker-compose"
+    "devcontainer"
+    "gh"
+    "git"
+    "nodejs"
+    "rtk"
+    "uv"
+  ];
+in
+{
   perSystem =
     { pkgs, ... }:
     let
@@ -6,6 +28,13 @@ _: {
     in
     {
       checks = {
+        evolver-developer-tools =
+          assert lib.all (name: lib.elem name packageNames) requiredPackageNames;
+          assert eVolver.virtualisation.docker.enable;
+          pkgs.runCommand "nix-arbor-evolver-developer-tools" { } ''
+            touch $out
+          '';
+
         nixfmt = pkgs.runCommand "nix-arbor-nixfmt" { } ''
           { find ${src}/config ${src}/modules -type f -name '*.nix' -print0; printf '%s\\0' ${src}/flake.nix; } \
             | xargs -0 ${pkgs.nixfmt}/bin/nixfmt --check
